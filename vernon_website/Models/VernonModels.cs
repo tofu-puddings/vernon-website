e33010b32a4c77381c4638851814681701812dfa
tofupuddings <242073A@mymail.nyp.edu.sg>
2026-09-08 17:16:53 +0800

    new version

---
+++ vernon_website/Models/VernonModels.cs
@@ -0,0 +1,17 @@
+﻿namespace VernonFanSite.Models
+{
+    public class FanMessage
+    {
+        public int Id { get; set; }
+        public string SenderName { get; set; }
+        public string MessageContent { get; set; }
+        public DateTime PostedAt { get; set; } = DateTime.Now;
+    }
+
+    // We will use this to pass data to the view
+    public class HomeViewModel
+    {
+        public List<FanMessage> Messages { get; set; }
+        public List<string> FunFacts { get; set; }
+    }
+}
---
+++ vernon_website/Views/Shared/_Layout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace vernon_website.Views.Shared
+{
+    public class _LayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
